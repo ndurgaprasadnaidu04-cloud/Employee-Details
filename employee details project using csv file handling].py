2f4772e6a194780011e3732461658386a95334fc
Durga prasad N <n.durgaprasadnaidu04@gmail.com>
2026-03-25 15:39:07 +0530

    Refactor employee details CSV handling code

--- employee details project using csv file handling].py
+++ employee details project using csv file handling].py
@@ -1,10 +1,3 @@
-#wap using csv file handling to store emp details .
-# the program should perform the following operations
-# 1)add emp details into csv file
-# 2) display all employees
-# 3)Calculate total annual salary all emp
-# 4)exit
-# note: Each employee should record must contain ("ename","salary","job","dept.no.","annual salary")
 
 
 import csv
